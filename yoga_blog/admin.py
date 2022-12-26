bff9f7c863ef63495a1d0217ef9dec1fa7b88be6
MartinaP87 <bleach1987@hotmail.it>
2022-12-26 21:56:42 +0000

    Create a post admin class in admin.py.

--- yoga_blog/admin.py
+++ yoga_blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'created_on', 'status')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
