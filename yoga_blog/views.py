fa64f519c1340203f9c622f5ad3f4ceab4598f41
MartinaP87 <bleach1987@hotmail.it>
2022-12-27 10:17:01 +0000

    Create forms.py and a comment form, add its validation to the post detail view, and render it in the template.

--- yoga_blog/views.py
+++ yoga_blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 
 def post_list(request):
@@ -29,7 +30,27 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.filter(approved=True).order_by("-created_on")
+    commented = False
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            messages.success(
+                request, 'You successfully left a comment.<br>\
+                Your comment is waiting for approval.')
+            commented = True
+        else:
+            comment_form = CommentForm()
     context = {
                 "post": post,
+                "comments": comments,
+                "commented": commented,
+                "comment_form": comment_form,
             }
     return render(request, "yoga_blog/post_detail.html", context)
