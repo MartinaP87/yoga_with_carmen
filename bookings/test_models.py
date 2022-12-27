3c198f671e57d3faecd338b4ff6aeee89c51fe62
MartinaP87 <bleach1987@hotmail.it>
2022-12-27 11:52:29 +0000

    Add booking models testing.

---
+++ bookings/test_models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from .models import YogaType, YogaClass, Reservation, Notes
+
+
+class TestModels(TestCase):
+
+    def yoga_type_status_default_to_zero(self):
+        yoga_type = YogaType.objects.create(title="TestYogaType")
+        self.assertEqual(yoga_type.status, 0)
+
+    def yoga_type_introduction_default_to_intro(self):
+        yoga_type = YogaType.objects.create(title="TestYogaType")
+        self.assertEqual(yoga_type.introduction, "intro")
+
+    def yoga_class_time_default_nine_to_ten(self):
+        yoga_type_test = YogaType.objects.create(title="TestYogaType")
+        yoga_class = YogaClass.objects.create(yoga_type=yoga_type_test)
+        self.assertEqual(yoga_class.time, "9:00 - 10:00")
+
+    def yoga_class_available_spaces_default_twenty(self):
+        yoga_type_test = YogaType.objects.create(title="TestYogaType")
+        yoga_class = YogaClass.objects.create(yoga_type=yoga_type_test)
+        self.assertEqual(yoga_class.available_spaces, 20)
+
+    def yoga_class_status_default_zero(self):
+        yoga_type_test = YogaType.objects.create(title="TestYogaType")
+        yoga_class = YogaClass.objects.create(yoga_type=yoga_type_test)
+        self.assertEqual(yoga_class.status, 0)
+
+    def reservation_approved_default_true(self):
+        yoga_type_test = YogaType.objects.create(title="TestYogaType")
+        yoga_class = YogaClass.objects.create(yoga_type=yoga_type_test)
+        user = User.objects.create(username='TestUsernameCooment')
+        reservation = Reservation.objects.create(
+            yoga_class=yoga_class, member=user)
+        self.assertTrue(reservation.approved)
+
+    def notes_annotation_default_remember(self):
+        yoga_type_test = YogaType.objects.create(title="TestYogaType")
+        yoga_class = YogaClass.objects.create(yoga_type=yoga_type_test)
+        user = User.objects.create(username='TestUsernameCooment')
+        reservation = Reservation.objects.create(
+            yoga_class=yoga_class, member=user)
+        note = Notes.objects.create(reservation=reservation)
+        self.assertEqual(note.annotation, "Remember...")
