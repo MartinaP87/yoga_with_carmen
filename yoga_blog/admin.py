84110b749d5154fa673c044432045cf3c6795a92
MartinaP87 <bleach1987@hotmail.it>
2022-12-27 10:24:14 +0000

    In admin.py create a comment admin model to allow the admin to approve comments.

--- yoga_blog/admin.py
+++ yoga_blog/admin.py
@@ -11,3 +11,15 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'created_on', 'status')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
